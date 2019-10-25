7f2a78ed8e7c3c805c84beee74bf138f8aac5e32
jlas <juan.lasheras@gmail.com>
2019-10-25 16:16:59 -0700

    [edge] Alternative sobel implementation

--- edge_detection/app.js
+++ edge_detection/app.js
@@ -11,6 +11,101 @@ function changeImage() {
     image.src = "/images/" + select.value;
 }
 
+function to2dArray(imageData) {
+    let chans = new Array(3);
+    for (let c = 0; c < 3; c += 1) {
+        chans[c] = new Array(imageData.height);
+    }
+    for (let y = 0; y < imageData.height; y += 1) {
+        for (let c = 0; c < 3; c += 1) {
+            chans[c][y] = new Array(imageData.width);
+        }
+        let yoffset = y * (imageData.width * 4);
+        for (let x = 0; x < imageData.width; x += 1) {
+            let xoffset = yoffset + x * 4;
+            chans[0][y][x] = imageData.data[xoffset];
+            chans[1][y][x] = imageData.data[xoffset + 1];
+            chans[2][y][x] = imageData.data[xoffset + 2];
+        }
+    }
+    return chans;
+}
+
+function filterYAlt(imageData2dArray, width, height, filter) {
+    let d = imageData2dArray;
+    let r = new Array(height);
+    let k = filter;
+    r[0] = new Array(width).fill(0);
+    r[height - 1] = new Array(width).fill(0);
+    for (let y = 1; y < height - 1; y += 1) {
+        r[y] = new Array(width);
+        for (let x = 0; x < width; x += 1) {
+            r[y][x] = d[y - 1][x] * k[0] + d[y][x] * k[1] + d[y + 1][x] * k[2];
+        }
+    }
+    return r;
+}
+
+function filterXAlt(imageData2dArray, width, height, filter) {
+    let d = imageData2dArray;
+    let r = new Array(height);
+    let k = filter;
+    for (let y = 0; y < height; y += 1) {
+        r[y] = new Array(width);
+        for (let x = 1; x < width - 1; x += 1) {
+            r[y][x] = d[y][x - 1] * k[0] + d[y][x] * k[1] + d[y][x + 1] * k[2];
+        }
+    }
+    return r;
+}
+
+function sobelAlt() {
+    let imageData = sourceCtx.getImageData(0, 0, source.width, source.height);
+    let width = imageData.width;
+    let height = imageData.height;
+
+    let chans = to2dArray(imageData);
+
+    // X direction
+    for (let c of chans) {
+        let filteredXY = filterYAlt(c, width, height, [1, 2, 1]);
+        let filteredXX = filterXAlt(filteredXY, width, height, [1, 0, -1]);
+
+        // Y direction
+        let filteredYX = filterXAlt(c, width, height, [1, 2, 1]);
+        let filteredYY = filterYAlt(filteredYX, width, height, [1, 0, -1]);
+
+        for (let y = 0; y < height; y += 1) {
+            for (let x = 0; x < width; x += 1) {
+                c[y][x] =
+                    Math.abs(filteredXX[y][x]) + Math.abs(filteredYY[y][x]);
+            }
+        }
+    }
+
+    let r = new Array(height);
+    for (let y = 0; y < height; y += 1) {
+        r[y] = new Array(width);
+        for (let x = 0; x < width; x += 1) {
+            r[y][x] = Math.max(chans[0][y][x], chans[1][y][x], chans[2][y][x]);
+        }
+    }
+
+    let newImage = targetCtx.createImageData(target.width, target.height);
+    let i = 0;
+    for (let y = 0; y < height; y += 1) {
+        for (let x = 0; x < width; x += 1) {
+            newImage.data[i] = r[y][x];
+            newImage.data[i + 1] = r[y][x];
+            newImage.data[i + 2] = r[y][x];
+            newImage.data[i + 3] = 255;
+            i += 4;
+        }
+    }
+
+    targetCtx.putImageData(newImage, 0, 0);
+}
+
 function filterY(imageDataData, width, height, filter) {
     let d = imageDataData;
     let r = new Array(d.length);
@@ -102,7 +197,9 @@ function initImage() {
     target.width = targetWidth;
     target.height = targetHeight;
 
-    sobel();
+    let d = new Date();
+    sobelAlt();
+    console.log("sobel time: ", new Date() - d);
 }
 
 // Initialize application
